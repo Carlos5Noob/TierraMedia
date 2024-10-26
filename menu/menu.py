e094397d51d7ca3d02eedb78b0df539678d07de8
Carlos5Noob <cchaati2101@fp.iesromerovargas.com>
2024-10-26 12:16:10 +0200

    menu added

---
+++ menu/menu.py
@@ -0,0 +1,11 @@
+def show_menu():
+    print("""\n1. Registrar un nuevo personaje
+2. Añadir equipamiento a un personaje
+3. Equipar un arma a un personaje
+4. Establecer relaciones entre personajes
+5. Mover un personaje a una nueva localización
+6. Simular una batalla entre dos personajes
+7. Listar personajes por facción
+8. Buscar personajes por equipamiento
+9. Mostrar todos los personajes
+10. Salir""")
