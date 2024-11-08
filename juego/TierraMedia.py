73cc4a5331d3b07255419fd7d376a7fe6417054b
Alvarokstar <afercal1704@fp.iesromerovargas.com>
2024-11-08 11:57:20 +0100

    POO "cambiar_ubicacion" y "añadir_relacion" commit and push

--- juego/TierraMedia.py
+++ juego/TierraMedia.py
@@ -1,7 +1,22 @@
+from relations import relations
+from character import Personaje
+from ubication import Ubication
+
 class TierraMedia:
+
     def __init__(self, personajes, facciones):
         self.personajes = personajes
         self.facciones = facciones
+        self.characters = {}
 
-    def registrar_personaje(self, nombre, raza, faccion, ubicacion):
-        nuevo_personaje
+    def añadir_relacion(self, nombre, relaciones, tipo_relacion, nivel_confianza):
+        nueva_relacion = relations.relations[nombre, tipo_relacion, nivel_confianza]
+        self.characters[nombre]["relacion"] = nueva_relacion
+
+        print(f"{nombre} relacionado con {nombre}, el tipo es {tipo_relacion} y su nivel de confianza es {nivel_confianza}")
+
+    def cambiar_ubicacion(self, nombre, ubicacion):
+        nueva_ubicacion = Ubication.Ubication[nombre, ubicacion]
+        self.characters[nombre]["tipo"] = nueva_ubicacion
+
+        print(f"{nombre} se ha desplazado a {ubicacion}")
